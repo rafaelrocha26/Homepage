eb814284891a32d1a18a0f352e8a80c58bf11cc9
rafaelrocha26 <105436857+rafaelrocha26@users.noreply.github.com>
2023-08-07 10:04:32 -0300

    config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
